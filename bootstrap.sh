963099ce15f76f5af31333e113c008737af2dfb3
Tyler <tyler@molamphy.net>
2022-11-13 20:26:44 -0700

    New approach https://thinkinfi.com/upload-and-display-image-in-flask-python/

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install vlc fbi python3-pip -y
+apt install vlc fbi python3-pip memcached libmemcached-tools -y
 pip3 install flask
 echo Done
 exit 0
--- src/app.py
+++ src/app.py
@@ -1,24 +1,51 @@
-from flask import Flask, render_template
-import subprocess
+from flask import Flask, render_template, request, session
 import os
-import datetime
-import time
+from werkzeug.utils import secure_filename
+ 
+#*** Backend operation
+ 
+# WSGI Application
+# Defining upload folder path
+UPLOAD_FOLDER = os.path.join('staticFiles', 'uploads')
+# # Define allowed files
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ 
+# Provide template folder name
+# The default folder name should be "templates" else need to mention custom folder name for template path
+# The default folder name for static files should be "static" else need to mention custom folder for static path
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+# Configure upload folder for Flask application
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-   now = datetime.datetime.now()
-   timeString = now.strftime("%Y-%m-%d %H:%M")
-   templateData = {
-      'title' : 'HELLO!',
-      'time': timeString
-      }
-   return render_template('main.html', **templateData)
-
-@app.route("/showimage/")
-def showimage(one, two):
-   subprocess.call(['./fbi.sh'], shell=True)
-   return showimage
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'c41FRlse6dhmD2zr52kdfTDAgtNejXjKDoQyCUiHXo7YeR'
+sess = Session()
+ 
+@app.route('/')
+def index():
+    return render_template('main.html')
+ 
+@app.route('/',  methods=("POST", "GET"))
+def uploadFile():
+    if request.method == 'POST':
+        # Upload file flask
+        uploaded_img = request.files['uploaded-file']
+        # Extracting uploaded data file name
+        img_filename = secure_filename(uploaded_img.filename)
+        # Upload file to database (defined uploaded folder in static path)
+        uploaded_img.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        # Storing uploaded file path in flask session
+        session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+ 
+        return render_template('success.html')
+ 
+@app.route('/show_image')
+def displayImage():
+    # Retrieving uploaded file path from session
+    img_file_path = session.get('uploaded_img_file_path', None)
+    # Display image in Flask application web page
+    return render_template('show_image.html', user_image = img_file_path)
+ 
+if __name__=='__main__':
+    app.run(debug = True)
