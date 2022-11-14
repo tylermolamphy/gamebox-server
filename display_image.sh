c9439bb19d8111c368a5b76e629ed6371c714b5e
Tyler <tyler@molamphy.net>
2022-11-13 22:58:42 -0700

    Pathless

--- display_image.sh
+++ display_image.sh
@@ -2,11 +2,11 @@
 /usr/bin/clear
 if [ $# -eq 0 ]
   then
-    sudo /usr/bin/killall /usr/bin/fbi
-    sudo /usr/bin/fbi -a --noverbose -T 1 /opt/uploaded_images/$(ls -t /opt/uploaded_images/ | head -1)
+    /usr/bin/sudo /usr/bin/killall /usr/bin/fbi
+    /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/uploaded_images/$(/usr/bin/ls -t /opt/uploaded_images/ | /usr/bin/head -1)
     exit 0
 fi
-sudo /usr/bin/killall /usr/bin/fbi
+/usr/bin/sudo /usr/bin/killall /usr/bin/fbi
 /usr/bin/sleep 1
-sudo /usr/bin/fbi -a --noverbose -T 1 $1
+/usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 $1
 exit 0
