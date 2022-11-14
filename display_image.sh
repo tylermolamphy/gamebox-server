00432088e877574ba5d812b62c3a25dd54b8cd2d
Tyler <tyler@molamphy.net>
2022-11-13 22:55:26 -0700

    No paths

--- display_image.sh
+++ display_image.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-clear
+/usr/bin/clear
 if [ $# -eq 0 ]
   then
-    sudo killall /usr/bin/fbi
+    sudo /usr/bin/killall /usr/bin/fbi
     sudo /usr/bin/fbi -a --noverbose -T 1 /opt/uploaded_images/$(ls -t /opt/uploaded_images/ | head -1)
     exit 0
 fi
-sudo killall /usr/bin/fbi
-sleep 1
+sudo /usr/bin/killall /usr/bin/fbi
+/usr/bin/sleep 1
 sudo /usr/bin/fbi -a --noverbose -T 1 $1
 exit 0
