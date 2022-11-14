d83cfd40a2927fa4ecdec1f2f6d041e2986caf82
Tyler Molamphy <dev>
2022-11-13 22:52:47 -0700

    works

--- display_image.sh
+++ display_image.sh
@@ -2,10 +2,11 @@
 clear
 if [ $# -eq 0 ]
   then
-    1 = $(ls -t /opt/uploaded_images/ | head -1)
+    sudo killall /usr/bin/fbi
+    sudo /usr/bin/fbi -a --noverbose -T 1 /opt/uploaded_images/$(ls -t /opt/uploaded_images/ | head -1)
+    exit 0
 fi
 sudo killall /usr/bin/fbi
 sleep 1
 sudo /usr/bin/fbi -a --noverbose -T 1 $1
-clear
 exit 0
