3cb6031d92df280cd9aad88ce7c3d5e9a4211b37
Tyler <tyler@molamphy.net>
2022-11-13 20:02:55 -0700

    Just kill and go

--- src/fbi.sh
+++ src/fbi.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
- 
-SERVICE='fbi'
-while true; do
-if ps ax | grep -v grep | grep $SERVICE > /dev/null
-then
-echo "running" # >> /dev/null
-else
-sudo fbi -a --noverbose -T 1 /home/t/image.jpg & 
-fi
+killall fbi
+sleep 1
+sudo fbi -a --noverbose -T 1 /home/t/image.jpg
 done
