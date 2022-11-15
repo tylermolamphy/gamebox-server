483f002ebce6d9bdafbfa3808ac2bb4f97e01609
Tyler <tyler@molamphy.net>
2022-11-14 21:39:45 -0700

    Add libjpeg

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install fbi python3-pip inotify-tools  -y
+apt install fbi libjpeg-dev python3-pip inotify-tools  -y
 pip3 install flask gunicorn
 reboot
 exit 0
