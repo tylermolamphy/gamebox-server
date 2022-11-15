dba77961f0673f3e7b0ab649e9fa248a67617f93
Tyler <tyler@molamphy.net>
2022-11-14 20:03:33 -0700

    Working

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = os.path.join('staticFiles', 'uploads')
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/opt/uploaded_images/'
 
 @app.route('/')
