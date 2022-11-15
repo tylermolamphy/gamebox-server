227bab189256072089839044251dd0c2077bf3e3
Tyler <tyler@molamphy.net>
2022-11-15 16:12:34 -0700

    Remove debugs

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,9 +7,7 @@ git pull --force
 popd
 /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
 /usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
-echo Made it to main loop
 while true
 do sleep 30
-echo "Check for updates..."
 git pull --force | grep forward && reboot 
 done
