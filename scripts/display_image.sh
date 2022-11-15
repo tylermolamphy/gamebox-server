d95a3e31664e0b00856de8eb0ca3f7589d5e2b6c
Tyler <tyler@molamphy.net>
2022-11-15 12:42:39 -0700

    Attempt scaling sides

--- scripts/display_image.sh
+++ scripts/display_image.sh
@@ -5,7 +5,7 @@ upMins=$((${upSeconds} / 60))
 if [ $# -eq 0 ]
   then
     /usr/bin/sudo /usr/bin/killall /usr/bin/fbi
-    /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/uploaded_images/$(/usr/bin/ls -t /opt/uploaded_images/ | /usr/bin/head -1)
+    /usr/bin/sudo /usr/bin/fbi -a --fitwidth --noverbose -T 1 /opt/uploaded_images/$(/usr/bin/ls -t /opt/uploaded_images/ | /usr/bin/head -1)
     exit 0
 fi
 /usr/bin/sudo /usr/bin/killall /usr/bin/fbi
