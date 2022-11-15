1d6f109bea86639f8efcfbb2c471a94956dc6854
Tyler <tyler@molamphy.net>
2022-11-14 21:43:38 -0700

    inotify no longer needed

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,8 +6,5 @@ git config pull.ff only
 git pull --force
 popd
 /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
-/usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
-while inotifywait -mre create,delete,modify /opt/gamebox-server ; do
-    sudo /sbin/reboot
-done
+/usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app
 exit 0
