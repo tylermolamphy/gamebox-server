b6943396b37f623029ecf87b7c74ce0abee59206
Tyler <tyler@molamphy.net>
2022-11-14 20:43:07 -0700

    No Verbose

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ pushd /opt/gamebox-server
 git config pull.ff only
 git pull --force
 popd
-/usr/bin/sudo /usr/bin/fbi -a -T 1 /opt/landing.jpg
+/usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
 /usr/local/bin/gunicorn -w 3 --bind unix:/tmp/gamebox-ipc.sock wsgi:app
 exit 0
