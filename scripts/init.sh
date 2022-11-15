ff3e5b0010d2a9ca6de167e4a000e580c29da156
Tyler <tyler@molamphy.net>
2022-11-14 21:04:23 -0700

    Remove edit link

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,7 +8,7 @@ popd
 /usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
 inotifywait -m /opt/gamebox-server -e create -e modify -e moved_to |
     while read path action file; do
-        killall gunicorn
+        pkill gunicorn
         sleep 5
         /usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
     done
