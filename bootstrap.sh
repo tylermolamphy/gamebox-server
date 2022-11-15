b047e910858090882f3b6998099006db9cfd93e5
Tyler <tyler@molamphy.net>
2022-11-14 20:31:43 -0700

    Changes for Dietpi in production

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install vlc fbi nginx python3-pip inotify-tools  -y
+apt install fbi python3-pip inotify-tools  -y
 pip3 install flask gunicorn
-curl -L https://bit.ly/glances | /bin/bash
 reboot
 exit 0
