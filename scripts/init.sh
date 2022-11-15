83d378074723497d06d3ed885e47fa81814d9c64
Tyler <tyler@molamphy.net>
2022-11-14 21:23:53 -0700

    Try paralell

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,9 +5,9 @@ git config pull.ff only
 git pull --force
 popd
 /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
+/usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
 inotifywait -m /opt/gamebox-server/ -e create -e modify -e moved_to |
     while read path action file; do
         halt --reboot
     done
-/usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app
 exit 0
