07b9488ce85e24ad43b0a7d63eb32ac3416a883a
Tyler <tyler@molamphy.net>
2022-11-14 20:42:18 -0700

    Changes for dietpi

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 date
-chown -R flask:www-data /opt/gamebox-server/
 pushd /opt/gamebox-server
 git config pull.ff only
 git pull --force
 popd
-chown -R flask:www-data /opt/gamebox-server/
-/usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
+/usr/bin/sudo /usr/bin/fbi -a -T 1 /opt/landing.jpg
+/usr/local/bin/gunicorn -w 3 --bind unix:/tmp/gamebox-ipc.sock wsgi:app
 exit 0
