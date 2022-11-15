25158cb2fb23c4f71cbb95cc9db885b69127e6f5
Tyler <tyler@molamphy.net>
2022-11-14 20:52:22 -0700

    Try a watch loop

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,5 +5,12 @@ git config pull.ff only
 git pull --force
 popd
 /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
-/usr/local/bin/gunicorn -w 3 --bind unix:/tmp/gamebox-ipc.sock wsgi:app
+/usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
+inotifywait -m /path -e create -e moved_to |
+    while read path action file; do
+        killall gunicorn
+        sleep 5
+        /usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
+    done
+
 exit 0
