8a79081fc01e134cd9c07eb80a0b681c5933f980
Tyler <tyler@molamphy.net>
2022-11-14 21:20:22 -0700

    Logic order

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,9 +5,9 @@ git config pull.ff only
 git pull --force
 popd
 /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
-inotifywait -m /opt/gamebox-server -e create -e modify -e moved_to |
+inotifywait -m /opt/gamebox-server/ -e create -e modify -e moved_to |
     while read path action file; do
-        sudo reboot
+        halt --reboot
     done
 /usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app
 exit 0
