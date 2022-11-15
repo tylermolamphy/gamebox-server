6baa54772d7b9a79148362815771eec3630251b8
Tyler <tyler@molamphy.net>
2022-11-14 21:16:21 -0700

    Looping

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,11 +5,9 @@ git config pull.ff only
 git pull --force
 popd
 /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
-/usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
 inotifywait -m /opt/gamebox-server -e create -e modify -e moved_to |
     while read path action file; do
-        pkill gunicorn
-        sleep 5
-        /usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
+        sudo reboot
     done
+/usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app
 exit 0
