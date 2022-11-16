f38fac4f6e47701fe422677ac735cef1e5eced7d
Tyler <tyler@molamphy.net>
2022-11-16 15:58:47 -0700

    Testing building libjpeg-turbo

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,4 +3,12 @@ apt update
 apt upgrade -y
 apt install fbi libjpeg-dev python3-pip glances -y
 pip3 install flask gunicorn bottle
+pushd /opt/
+git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git
+pushd /opt/libjpeg-turbo
+make
+make deb
+exit 0
+popd
+popd
 reboot
