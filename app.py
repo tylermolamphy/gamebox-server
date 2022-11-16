2b5010308cfeda34680f469d7c878934e9325c41
Tyler <tyler@molamphy.net>
2022-11-15 19:50:30 -0700

    Attempt to pre-declare variable on first run

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/opt/uploaded_images/'
+img_filename = 'landing.jpg'
 
 @app.route('/')
 def index():
