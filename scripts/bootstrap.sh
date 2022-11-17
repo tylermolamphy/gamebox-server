c3fa13235cc0b0e46b3d6fe81cbe0e9396409ff8
Tyler <tyler@molamphy.net>
2022-11-16 18:22:03 -0700

    Streamline setup

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+set -e
 apt update
 apt upgrade -y
-apt install build-essential cmake fbi libjpeg-dev python3-pip glances -y
+apt install build-essential cmake git python3-pip nginx fbi libjpeg-dev glances -y
 pip3 install flask gunicorn bottle
 pushd /opt/
 git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git
 pushd /opt/libjpeg-turbo
-make
-make deb
-exit 0
-popd
-popd
-reboot
+make && make deb
+dpkg -i `ls libjpeg-turbo*.deb`
+echo sleep 5 > /var/lib/dietpi/dietpi-autostart/custom.sh
+echo cd /opt/gamebox-server >> /var/lib/dietpi/dietpi-autostart/custom.sh
+echo /opt/gamebox-server/scripts/init.sh >> /var/lib/dietpi/dietpi-autostart/custom.sh
+echo exit 0 >> /var/lib/dietpi/dietpi-autostart/custom.sh
+chmod +x /var/lib/dietpi/dietpi-autostart/custom.sh
+pushd /etc/nginx
+ln -s /opt/gamebox-server/sites-available ./sites-enabled
+nginx -t
+popd ; popd ; popd ; reboot
