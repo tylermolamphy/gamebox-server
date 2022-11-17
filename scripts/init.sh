0a62d183d9073b10cbb5ccaffb081a5661638492
Tyler <tyler@molamphy.net>
2022-11-16 19:05:53 -0700

    Attempt to color temp

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,9 +3,33 @@ date
 whoami
 updatestatline() {
 uptime --pretty > /var/log/systemuptime.log
-/usr/bin/vcgencmd measure_temp | awk -F "[=']" {'print "temp " $2 " °C"'} > /var/log/systemtemp.log
 git rev-parse --short HEAD > /var/log/gameboxcommit.log
 df -h  | grep \/root | awk {'print $5'} > /var/log/spaceused.log
+temp=`/usr/bin/vcgencmd measure_temp | awk -F "[=']" {'print $2'} | cut -c 1-2`
+temp_f=`echo "scale=4; $temp*1.8 + 32" | bc`
+if (( $temp >= 70 ))
+then
+    temp="<p class="text-danger">WARNING: $temp °C / $temp_f °F : Reducing the life of your device</p>"
+
+elif (( $temp >= 60 ))
+then
+    temp="<p class="text-warning">$temp °C / $temp_f °F: Running hot, not recommended</p>"
+
+elif (( $temp >= 50 ))
+then
+    temp="<p class="text-light">$temp °C / $temp_f °F: Running warm, but safe</p>"
+
+elif (( $temp >= 40 ))
+then
+    temp="<p class="text-light">$temp °C / $temp_f °F: Optimal temperature</p>"
+
+elif (( $temp >= 30 ))
+then
+    temp="<p class="text-info">$temp °C / $temp_f °F: Suspiciously cool</p>"
+else
+    temp="<p class="text-danger">OUT OF RANGE: $temp °C / $temp_f °F: Something is very, very wrong</p>"
+fi
+echo $temp > /var/log/systemtemp.log
 }
 updatestatline
 pushd /opt/gamebox-server
