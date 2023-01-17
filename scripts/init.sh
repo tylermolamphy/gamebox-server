16b71221d0da7831fa03d110b7c55f8e31dc7454
tylermolamphy <spam@tylermolamphy.com>
2023-01-17 11:29:05 -0700

    Double time for update check loop

--- scripts/init.sh
+++ scripts/init.sh
@@ -38,6 +38,6 @@ popd
 /usr/bin/sudo /usr/bin/fbi -a --noverbose -T 1 /opt/landing.jpg
 /usr/local/bin/gunicorn -w 2 --bind unix:/tmp/gamebox-ipc.sock wsgi:app &disown
 while true
-do sleep 60
+do sleep 120
 updatestatline ; git pull --force | grep Updating && /sbin/reboot
 done
